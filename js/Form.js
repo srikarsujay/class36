5c3764a98d2f26806ab841a47ffc5a7350c1f2e3
srikarsujay <70843622+srikarsujay@users.noreply.github.com>
2020-11-18 18:56:29 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,37 @@
+class Form {
+  constructor() {
+   this.input=createInput('')
+   this.button=createButton("start")
+   this.greeting=createElement('h2') 
+  }
+  hide(){
+    this.button.hide()
+    this.greeting.hide()
+    this.input.hide()
+  }
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(130, 0);
+    
+  
+   this.input.position(130, 160);
+    this.button.position(250, 200);
+
+    this.button.mousePressed(()=>{
+     this.input.hide();
+      this.button.hide();
+
+   player.name =this.input.value();
+    
+      playerCount+=1;
+      player.index=playerCount
+      player.update()
+      player.updateCount(playerCount);
+      
+     this.greeting.html("Hello " + name )
+      this.greeting.position(130, 160)
+    });
+
+  }
+}
---
+++ js/Game.js
@@ -0,0 +1,51 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+  }
+  play(){
+    form.hide()
+    textSize(30)
+    text('GAME START',120,100)
+    Player.getPlayerInfo()
+    if(allPlayers!==undefined){
+      var dp=130
+      for(var plr in allPlayers){
+        if(plr==="player"+player.index){
+          fill("red")
+        }
+        else{
+          fill("black")
+        }
+        dp+=30
+        textSize(15)
+        text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,dp)
+      }
+    }
+    if(keyIsDown(UP_ARROW)&&player.index!==null){
+      player.distance+=50
+      player.update()
+    }
+  }
+
+}
